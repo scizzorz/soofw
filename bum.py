8f6bed3e42cafdc2b99e3f9f93d9b0da67306106
John Weachock <jweachock@gmail.com>
2013-07-22 01:07:53 -0400

    Change wildcard bumpy import and add @generates decorators

--- bum.py
+++ bum.py
@@ -1,40 +1,37 @@
-#!/usr/bin/env python
-import soofw
-from bumpy import *
+import bumpy, soofw
 
-@task
+@bumpy.task
 def clean():
 	'Removes all generated files.'
-	shell('rm -rf soofw/static/*.css')
-	shell('rm -rf *.pyc')
-	shell('rm -rf soofw/*.pyc')
-	shell('rm -rf soofw/content/links.md')
+	bumpy.shell('rm -rf *.pyc soofw/*.pyc')
+	bumpy.clean()
 
-@requires('soofw/scss/main.scss', 'soofw/scss/_include.scss')
+@bumpy.requires('soofw/scss/main.scss', 'soofw/scss/_include.scss')
+@bumpy.generates('soofw/static/main.css')
 def css():
 	'Compiles the SCSS into CSS.'
 
 	# only build CSS if the SCSS is newer
-	if age('soofw/scss/main.scss', 'soofw/scss/_include.scss') < age('soofw/static/main.css'):
+	if bumpy.age('soofw/scss/main.scss', 'soofw/scss/_include.scss') < bumpy.age('soofw/static/main.css'):
 		import scss
 		_scss = scss.Scss({}, {'compress': False, 'debug_info': False})
 
 		main_file = open('soofw/static/main.css', 'w')
 		main_file.write(_scss.compile(scss_file = 'soofw/scss/main.scss'))
 
-@task
+@bumpy.generates('soofw/content/links.md')
 def links():
 	'Generates the "links" page.'
 	import fetch_delicious
 	fetch_delicious.main()
 
-@default
-@requires(css, links)
+@bumpy.default
+@bumpy.requires(css, links)
 def all():
 	'Generates all necessary files.'
 	pass
 
-@task
+@bumpy.task
 def run():
 	'Runs the server.'
 	soofw.app.run(host='0.0.0.0', debug=True)
