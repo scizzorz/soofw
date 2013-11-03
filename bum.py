9834027c5d18fa07b83e484a7bedaeaa685ba5a2
John Weachock <jweachock@gmail.com>
2013-11-03 15:45:04 -0500

    Update bumpy to 0.4.3

--- bum.py
+++ bum.py
@@ -2,42 +2,34 @@ import sys, bumpy as b
 if '.' not in sys.path: sys.path.append('.')
 import soofw
 
-@b.generates('*.pyc **/*.pyc')
+@b.task(gens='*.pyc **/*.pyc')
 def clean():
 	'''Remove all generated files.'''
-
 	b.clean()
 
-@b.method
-@b.requires('soofw/scss/main.scss', 'soofw/scss/_include.scss')
-@b.generates('soofw/static/main.css')
+@b.task('method', reqs=('soofw/scss/main.scss', 'soofw/scss/_include.scss'), gens='soofw/static/main.css')
 def css(self):
 	'''Compile the SCSS into CSS.'''
-
 	# only build CSS if the SCSS is newer
-	if b.age(*self.file_requirements) < b.age(self.generates):
+	if b.age(*self.file_reqs) < b.age(self.gens):
 		import scss
 		_scss = scss.Scss({}, {'compress': False, 'debug_info': False})
 
-		main_file = open(self.generates, 'w')
-		main_file.write(_scss.compile(scss_file=self.file_requirements[0]))
+		main_file = open(self.gens, 'w')
+		main_file.write(_scss.compile(scss_file=self.file_reqs[0]))
 
-@b.generates('soofw/content/links.yml')
+@b.task(gens='soofw/content/links.yml')
 def links():
 	'''Generate the "links" page.'''
-
 	import fetch_delicious
 	fetch_delicious.main()
 
-@b.default
-@b.requires(css, links)
+@b.task('default', reqs=(css, links))
 def all():
 	'''Generate all necessary files.'''
-
 	pass
 
 @b.task
 def run():
 	'''Run the server.'''
-
 	soofw.app.run(host='0.0.0.0', debug=True)
